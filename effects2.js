2d84816e58be881640eac7d00d2b64fbc6568db5
die <139068281+sokufps@users.noreply.github.com>
2025-03-15 18:38:38 -0500

    Add files via upload

---
+++ effects2.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const imgSrc = "https://lanyard.cnrad.dev/api/1048691978285621268?bg=00000000&animated=true=true&hideSpotify=false&showDisplayName=true&hideActivity=true&hideBadges=true";
+    const imgElement = document.getElementById('rpc');
+    imgElement.src = imgSrc;
+});
